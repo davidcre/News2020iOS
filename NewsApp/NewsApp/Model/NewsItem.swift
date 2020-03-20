bd5c17905d19f1e0c9684dd72dde4379f4b9157f
Josselin Duboc <josselin.duboc@orange.com>
2020-03-20 11:10:20 +0100

    Page à la une topFR

---
+++ NewsApp/NewsApp/Model/NewsItem.swift
@@ -0,0 +1,38 @@
+//
+//  NewsItem.swift
+//  NewsApp
+//
+//  Created by Josselin DUBOC on 19/03/2020.
+//  Copyright © 2020 Josselin DUBOC. All rights reserved.
+//
+
+class NewsItem {
+
+    var author: String!
+    var title: String!
+    var descriptionTitle: String!
+    var publishedAt: String!
+    var urlToImage: String!
+    var url: String!
+    var content: String!
+
+    init() {
+
+    }
+
+    init(author: String? = "", title: String? = "", descriptionTitle: String? = "", publishedAt: String? = "", urlToImage: String? = "", url: String? = "", content: String? = "") {
+        self.author = author
+        self.title = title
+        self.descriptionTitle = descriptionTitle
+        self.publishedAt = publishedAt
+        self.urlToImage = urlToImage
+        self.url = urlToImage
+        self.content = content
+    }
+
+    func description() -> String {
+        return ("Author: \(author ?? ""), title: \(title ?? ""), descriptionTitle: \(descriptionTitle ?? ""), ") +
+        "publishedAt: \(publishedAt ?? ""), urlToImage: \(urlToImage ?? ""), url: \(url ?? ""), content: \(content ?? "") ."
+    }
+
+}
