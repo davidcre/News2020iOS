3daf5d97f352e1ebbb930b685157526f419bf749
Josselin Duboc <josselin.duboc@orange.com>
2020-04-08 15:53:09 +0200

    nouvelle vue recherche

--- NewsApp/NewsApp/Controller/SearchController.swift
+++ NewsApp/NewsApp/Controller/SearchController.swift
@@ -9,40 +9,50 @@
 import Foundation
 import UIKit
 
-class SearchController: UIViewController {
+class SearchController: UITableViewController {
     @IBOutlet private weak var searchBar: UISearchBar!
-    @IBOutlet private weak var dateButton: UIButton!
     @IBOutlet private weak var sortByLabel: UILabel!
     @IBOutlet private weak var sortBySegmentedControl: UISegmentedControl!
-    @IBOutlet private weak var dateFromLabel: UILabel!
-    @IBOutlet private weak var dateFromPicker: UIDatePicker!
-    @IBOutlet private weak var dateToLabel: UILabel!
-    @IBOutlet private weak var dateToPicker: UIDatePicker!
+    @IBOutlet private weak var dateFromLabelWithTitle: UILabel!
+    @IBOutlet private weak var dateFromLabelWithDate: UILabel!
+    @IBOutlet private weak var dateToLabelWithTitle: UILabel!
+    @IBOutlet private weak var dateToLabelWithDate: UILabel!
+    @IBOutlet private weak var languageLabelWithTitle: UILabel!
+    @IBOutlet private weak var languageLabelWithLanguage: UILabel!
+    @IBOutlet private weak var searchButton: UIButton!
+    @IBOutlet private weak var moreFiltersButton: UIButton!
+    @IBOutlet private weak var moreFiltersImage: UIImageView!
 
+    private var showMoreFilters: Bool = false
     private let sortBy = [SortBy(title: R.string.localizable.publicationDate(), sortByType: .publishedAt),
                           SortBy(title: R.string.localizable.popularity(), sortByType: .popularity),
                           SortBy(title: R.string.localizable.relevancy(), sortByType: .relevancy)]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        showOrNotMoreFilters(show: false)
         initSearchController()
     }
 
-    @IBAction func onDateButtonClicked() {
-        //searchBar.frame.origin.y += 150
-        dateFromLabel.isHidden = !dateFromLabel.isHidden
-        dateFromPicker.isHidden = !dateFromPicker.isHidden
-        dateToLabel.isHidden = !dateToLabel.isHidden
-        dateToPicker.isHidden = !dateToPicker.isHidden
+    @IBAction func onMoreFiltersButtonClicked() {
+        showOrNotMoreFilters(show: !showMoreFilters)
+        switch showMoreFilters {
+        case true:
+            moreFiltersButton.setImage(UIImage(systemName: "chevron.up"), for: .normal)
+        case false:
+            moreFiltersButton.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+        }
     }
 
     func initSearchController() {
         self.navigationItem.title = R.string.localizable.search()
         self.searchBar.placeholder = R.string.localizable.searchForAnArticle()
-        self.dateButton.setTitle(R.string.localizable.searchByDate(), for: .normal)
-        self.dateFromLabel.text = R.string.localizable.startDate()
-        self.dateToLabel.text = R.string.localizable.endDate()
         self.sortByLabel.text = R.string.localizable.sortBy()
+        self.languageLabelWithTitle.text = R.string.localizable.language()
+        self.dateFromLabelWithTitle.text = R.string.localizable.startDate()
+        self.dateToLabelWithTitle.text = R.string.localizable.endDate()
+        self.searchButton.setTitle(R.string.localizable.searchVerb(), for: .normal)
+        self.moreFiltersButton.setTitle(R.string.localizable.moreFilters(), for: .normal)
         initSegmentedControl()
     }
 
@@ -60,21 +70,40 @@ class SearchController: UIViewController {
         }
     }
 }
-//TODO: Un seul mot doit être tapé
-extension SearchController: UISearchBarDelegate {
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard searchBar.text != "" else {
-            return
-        }
-        let parametersRequest: ParametersRequest = ParametersRequest(query: searchBar.text, requestType: .everything)
-        performSegue(withIdentifier: R.segue.searchController.segueToNews, sender: parametersRequest)
-    }
-}
 
 extension SearchController {
     func initSegmentedControl() {
         for sortByIndice in 0..<self.sortBy.count {
             self.sortBySegmentedControl.setTitle(self.sortBy[sortByIndice].title, forSegmentAt: sortByIndice)
         }
+    }
+}
+
+//TODO: Un seul mot doit être tapé
+extension SearchController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard searchBar.text != "" else {
+            return
+        }
+        let parametersRequest: ParametersRequest = ParametersRequest(query: searchBar.text, sortBy: sortBy[sortBySegmentedControl.selectedSegmentIndex], requestType: .everything)
+        performSegue(withIdentifier: R.segue.searchController.segueToNews, sender: parametersRequest)
+    }
+}
+
+extension SearchController {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        var height: CGFloat
+        if !self.showMoreFilters && (indexPath.row == 3 || indexPath.row == 4 || indexPath.row == 5) {
+            height = 0.0
+        } else {
+            height = 45.0
+        }
+        return height
+    }
+
+    func showOrNotMoreFilters(show: Bool) {
+        self.showMoreFilters = show
+        tableView.beginUpdates()
+        tableView.endUpdates()
     }
 }
